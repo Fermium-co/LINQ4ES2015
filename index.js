dad0b0f08f2f8c7eb4d08e3e681915f44b32a351
Yasser Moradi <y.moradi@outlook.com>
2015-08-10 13:53:25 +0430

    Enhanced variable namings

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ linq();
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-let oddWhereExecutionTime = 0;
+let oddWhereExecutionCount = 0;
 
-let odd = arr.asEnumerable().where(n => {
-	oddWhereExecutionTime++;
+let odds = arr.asEnumerable().where(n => {
+	oddWhereExecutionCount++;
 	return n % 2 !== 0;
 }).take(2).toArray();
 
-let evenWhereExecutionTime = 0;
+let evenWhereExecutionCount = 0;
 
-let even = arr.asEnumerable().where(n => {
-	evenWhereExecutionTime++;
+let evens = arr.asEnumerable().where(n => {
+	evenWhereExecutionCount++;
 	return n % 2 === 0;
 }).take(2).toArray();
 
 console.log('array: ' + arr);
-console.log('odds: ' + odd);
-console.log('evens: ' + even);
-console.log('oddWhereExecutionTime: ' + oddWhereExecutionTime);
-console.log('evenWhereExecutionTime: ' + evenWhereExecutionTime);
+console.log('odds: ' + odds);
+console.log('evens: ' + evens);
+console.log('oddWhereExecutionCount: ' + oddWhereExecutionCount);
+console.log('evenWhereExecutionCount: ' + evenWhereExecutionCount);
