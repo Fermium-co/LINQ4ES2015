a6992257c52662dfd1c2109b85246b2de0b1891b
Yasser Moradi <y.moradi@outlook.com>
2015-08-12 16:55:40 +0430

    prevent enumerable from being enumerated twice

--- tests/modules/asEnumerable.spec.js
+++ tests/modules/asEnumerable.spec.js
@@ -7,7 +7,7 @@ import linq from "../../src/linq";
 import asEnumerable from "../../src/modules/asEnumerable";
 
 describe("asEnumerable", () => {
-  
+
   it("must throws an exception when the source is null or undefined", () => {
     expect(() => asEnumerable(null).toArray()).toThrowError("source is null or undefined");
     expect(() => asEnumerable(undefined).toArray()).toThrowError("source is null or undefined");
@@ -16,4 +16,9 @@ describe("asEnumerable", () => {
   it("must throws an exception when the source can not be enumerated", () => {
     expect(() => asEnumerable({}).toArray()).toThrowError("source can not be enumerated");
   });
+
+  it("must throws an exception when the source is enumerable", () => {
+    expect(() => asEnumerable((function* () { })()).toArray()).toThrowError("enumerable may not be enumerated twice");
+  });
+
 });
