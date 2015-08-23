354f658091f603876896c09852826c7dc02999a0
Amin Sheikhi <AminSheikhi@gmail.com>
2015-08-23 12:55:18 +0430

    SelectMany method `s description  added

--- samples/index.js
+++ samples/index.js
@@ -1,4 +1,4 @@
-import Linq from "Fermium-co/LINQ4ES2015/linq";
+import Linq from 'Fermium-co/LINQ4ES2015/linq';
 Linq.setExtensions();
 
 let count = 0;
@@ -12,58 +12,80 @@ let result = [0, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9].asEnumerable()
 
 console.log(result);
 console.log(count);
-// Where
-// One of the main tasks of a LINQ query is to restrict the results from a larger
-// collection based on some criteria. This is achieved using the Where operator,
-// which tests each element within a source collection and returns only those
-// elements that return a true result when tested against a given predicate
-// expression. A predicate is simply an expression that takes an element of the
-// same type of the items in the source collection and returns true or false. 
+/* Where
+One of the main tasks of a LINQ query is to restrict the results from a larger
+collection based on some criteria. This is achieved using the Where operator,
+which tests each element within a source collection and returns only those
+elements that return a true result when tested against a given predicate
+expression. A predicate is simply an expression that takes an element of the
+same type of the items in the source collection and returns true or false.*/
 
-var animals = ["Koala", "Kangaroo","Spider", "Wombat","Snake","Emu","Shark","Sting-Ray","Jellyfish"];
-var q = animals.asEnumerable()
-  .where(a=> a.indexOf("S") === 0 && a.length > 5)
+let animals = ['Koala', 'Kangaroo', 'Spider', 'Wombat', 'Snake', 'Emu', 'Shark', 'Sting-Ray', 'Jellyfish'];
+let q = animals.asEnumerable()
+  .where(a=> a.indexOf('S') === 0 && a.length > 5)
   .toArray();
 
 console.log('using where clause ...');
 
-for (var index = 0; index < q.length; index++) 
+for (let index = 0; index < q.length; index++)
   console.log(q[index]);
   
-  //Select
-  // The process of transforming the results of a query is called projection.
-  //  You can project the results of a query after any filters have been applied 
-  //  to change the type of the collection that is returned. For example, you can 
-  //  select a single property or field from the source data or project multiple 
-  //  properties into an type. You can also add calculations and other 
-  //  operations to the projection to generate information that is based upon the 
-  //  source data but not directly retrieved from it.
+/*Select
+The process of transforming the results of a query is called projection.
+You can project the results of a query after any filters have been applied 
+to change the type of the collection that is returned. For example, you can 
+select a single property or field from the source data or project multiple 
+properties into an type. You can also add calculations and other 
+operations to the projection to generate information that is based upon the 
+source data but not directly retrieved from it.*/
+
+let employees = [
+  { name: 'Bob', title: 'Senior Developer', salary: 40000 },
+  { name: 'Sam', title: 'Developer', salary: 32000 },
+  { name: 'Mel', title: 'Developer', salary: 29000 },
+  { name: 'Jim', title: 'Junior Developer', salary: 20000 },
+];
+
+let names = employees.asEnumerable()
+  .select(emp => emp.name)
+  .toArray();
+
+console.log('*** using select method to project name property ...');
+for (let index = 0; index < names.length; index++)
+  console.log(names[index]);
+
+
+let salaryIncrease = employees.asEnumerable()
+  .select(emp =>
+    'Employee : ' + emp.name +
+    ' Salary : ' + emp.salary +
+    ' New salary : ' + emp.salary * 1.05)
+  .toArray();
+
+console.log('*** projection with calculation ... ');
+
+for (let index = 0; index < salaryIncrease.length; index++)
+  console.log(salaryIncrease[index]);
   
-  var employees = [
-    {name: "Bob", title: "Senior Developer", salary: 40000},
-    {name: "Sam", title: "Developer",        salary: 32000},
-    {name: "Mel", title: "Developer",        salary: 29000},
-    {name: "Jim", title: "Junior Developer", salary: 20000},
-  ];
-  
-  var names = employees.asEnumerable()
-    .select(emp => emp.name)
-    .toArray();
-    
-    console.log("using select method to project name property ...");
-    for (var index = 0; index < names.length; index++)
-      console.log(names[index]);
-      
-      
-      var salaryIncrease = employees.asEnumerable()
-      .select(emp => 
-         "Employee : " + emp.name + 
-          " Salary : " + emp.salary + 
-          " New salary : " + emp.salary * 1.05)
-          .toArray();
-      
-      console.log("projection with calculation ... ");
-      
-      for (var index = 0; index < salaryIncrease.length; index++)
-        console.log(salaryIncrease[index]);
-  
+/*Select Many
+  This method `s parameter is used to extract a child collection from each parent item.
+  Finally all of the child collection are combined , or flattened , into a single array.
+  Let`s extend above sample empployees list that employee objects include a collection of strings
+  holding the skills. When the SelectMany method is used to read the Skills collections , the four 
+  set of skills are extracted and the combined into a single array.
+*/
+
+let employeesIncudeSkills = [
+  { name: 'Bob', title: 'Senior Developer', salary: 40000, skills: ['ASP.NET', 'C#', 'JavaScript', 'SQL', 'XML'] },
+  { name: 'Sam', title: 'Developer', salary: 32000, skills: ['ASP.NET', 'C#', 'Oracle', 'XML'] },
+  { name: 'Mel', title: 'Developer', salary: 29000, skills: ['C#', 'C++', 'SQL'] },
+  { name: 'Jim', title: 'Junior Developer', salary: 20000, skills: ['HTML', 'Visual Basic'] }
+];
+
+let skills = employeesIncudeSkills.asEnumerable()
+  .selectMany(emp => emp.skills)
+  .toArray();
+
+console.log("*** using selectMany method to combine skills ...");
+for (let index = 0; index < skills.length; index++)
+  console.log(skills[index]);
