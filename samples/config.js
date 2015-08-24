a3cbf1365ed15d3f6be1ec2f29e15c96c110c65c
Yasser Moradi <y.moradi@outlook.com>
2015-08-24 11:21:39 +0430

    Updated babel & core js, added jspm as dev dependency, comment some codes in distinct. Fixed a problem in tests.html

--- samples/config.js
+++ samples/config.js
@@ -1,35 +1,33 @@
 System.config({
-  "baseURL": "./",
-  "transpiler": "babel",
-  "babelOptions": {
+  baseURL: "./",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
     "optional": [
       "runtime"
     ]
   },
-  "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
-  }
-});
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
 
-System.config({
-  "map": {
+  map: {
     "Fermium-co/LINQ4ES2015": "github:Fermium-co/LINQ4ES2015@0.0.1",
-    "babel": "npm:babel-core@5.8.20",
+    "babel": "npm:babel-core@5.8.22",
     "babel-runtime": "npm:babel-runtime@5.8.20",
-    "core-js": "npm:core-js@0.9.18",
+    "core-js": "npm:core-js@1.1.1",
+    "linq4es2015": "github:Fermium-co/LINQ4ES2015@0.0.1",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:babel-runtime@5.8.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:core-js@0.9.18": {
+    "npm:core-js@1.1.1": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     }
   }
 });
-
--- src/modules/distinct.js
+++ src/modules/distinct.js
@@ -4,10 +4,10 @@ import utils from "./utils";
 
 export default function* (source, comparer) {
   if (this !== undefined && this !== null && arguments.length < 2) {
-    if (!utils.isGenerator(this) && (source instanceof Function)) {
+    //if (!utils.isGenerator(this) && (source instanceof Function)) {
       comparer = source;
       source = this;
-    }
+    //}
   }
   if (source == null || source == undefined) {
     throw new Error("source is null or undefined");
